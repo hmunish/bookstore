af2bcaeb771e4231bb5cf783151be7f56965b139
Munish <hmunish5@gmail.com>
2023-07-17 20:05:24 +0200

    Add actions for removing book from the state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,44 +1,44 @@
-import { v4 as uuidv4 } from 'uuid';
-import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from "uuid";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: [
     {
       item_id: uuidv4(),
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
       completed: 50,
     },
     {
       item_id: uuidv4(),
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
       completed: 75,
     },
     {
       item_id: uuidv4(),
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
       completed: 10,
     },
   ],
 };
 
 export const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, bookDetails) => {
       // Adds book on the state
       state.value.push(bookDetails.payload);
     },
-    removeBook: (state, bookDetails) => {
+    removeBook: (state, bookId) => {
       // Removes book from the state
       state.value = state.value.filter(
-        (book) => book.bookName !== bookDetails.payload.bookName,
+        (book) => book.item_id !== bookId.payload
       );
     },
   },
