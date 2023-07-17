7372a011973f698147a227a57f87d18b2d566b28
Munish <hmunish5@gmail.com>
2023-07-17 15:05:32 +0200

    Add reducers for categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksSlices from './books/booksSlice';
+import categoriesSlices from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     books: booksSlices,
+    category: categoriesSlices,
   },
 });
 
