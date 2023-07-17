0076bedca7a73df0d72379a0e041499665a88f99
Munish <hmunish5@gmail.com>
2023-07-17 20:07:28 +0200

    Fix eslint errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,34 +1,34 @@
-import { v4 as uuidv4 } from "uuid";
-import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from 'uuid';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   value: [
     {
       item_id: uuidv4(),
-      title: "The Great Gatsby",
-      author: "John Smith",
-      category: "Fiction",
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
       completed: 50,
     },
     {
       item_id: uuidv4(),
-      title: "Anna Karenina",
-      author: "Leo Tolstoy",
-      category: "Fiction",
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
       completed: 75,
     },
     {
       item_id: uuidv4(),
-      title: "The Selfish Gene",
-      author: "Richard Dawkins",
-      category: "Nonfiction",
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
       completed: 10,
     },
   ],
 };
 
 export const booksSlice = createSlice({
-  name: "books",
+  name: 'books',
   initialState,
   reducers: {
     addBook: (state, bookDetails) => {
@@ -38,7 +38,7 @@ export const booksSlice = createSlice({
     removeBook: (state, bookId) => {
       // Removes book from the state
       state.value = state.value.filter(
-        (book) => book.item_id !== bookId.payload
+        (book) => book.item_id !== bookId.payload,
       );
     },
   },
